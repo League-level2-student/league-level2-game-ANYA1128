0388367b87a22f554df61b6c96648857c7eb2fdf
league <league@imac-17.attlocal.net>
2020-01-31 18:27:55 -0800

    Finish Next Game

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,7 @@ public class Game {
 	JFrame frame = new JFrame();
 	public static final int WIDTH = 1400;
 	public static final int HEIGHT = 1000;
-	GamePanel gPanel = new GamePanel();
+	GamePanel gPanel= new GamePanel();
 	
 	public static void main(String[] args) {
 	Game dog = new Game();
@@ -15,10 +15,12 @@ public class Game {
 		
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setVisible(true);
+		
+	
 		frame.add(gPanel);
 	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		frame.addKeyListener(gPanel);
 	}
 
 	private void setDefaultCloseOperation(int exitOnClose) {
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,13 +7,11 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
-
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
-
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	final int MENU = 0;
@@ -25,21 +23,25 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer timer;
 	public static BufferedImage image;
 	public static BufferedImage image2;
+	public static BufferedImage image3;
 	public static boolean needImage = true;
 	public static boolean gotImage = false;
-
 
 	Font titleFont = new Font("Comic Sans", Font.PLAIN, 48);
 	Font instructionsFont = new Font("Comic Sans", Font.PLAIN, 23);
 	Font scoreFont = new Font("Comic Sans", Font.PLAIN, 35);
 
-	GamePanel(){
+	GamePanel() {
 		loadImage();
+		//frame.addKeyListener(this);
+		//repaint();
+		//startGame();
 	}
+
 	public void startGame() {
-		 timer = new Timer(1000/60,this);
-		 timer.start();
-		
+		timer = new Timer(1000 / 60, this);
+		timer.start();
+
 	}
 
 	public void paintComponent(Graphics g) {
@@ -54,13 +56,12 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		}
 	}
 
-		
-
 	void loadImage() {
 		if (needImage) {
 			try {
 				image = ImageIO.read(this.getClass().getResourceAsStream("background.jpg"));
-				image2 = ImageIO.read(this.getClass().getResourceAsStream("images-1.png"));
+				image2 = ImageIO.read(this.getClass().getResourceAsStream("download.png"));
+				image3 = ImageIO.read(this.getClass().getResourceAsStream("river.jpg"));
 				gotImage = true;
 			} catch (Exception e) {
 
@@ -69,11 +70,8 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		}
 	}
 
-
 	void updateMenuState() {
 	}
-
-	
 
 	void updateEndState() {
 	}
@@ -81,24 +79,24 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	void drawMenuState(Graphics g) {
 		if (gotImage) {
 			g.drawImage(image, 0, 0, Game.WIDTH, Game.HEIGHT, null);
-			g.drawImage(image2, 50, 900, 50, 50, null);
+			g.drawImage(image2, 400, 600, 200, 200, null);
 			g.setFont(titleFont);
 			g.setColor(Color.BLACK);
 			g.drawString("INSERT GAME NAME HERE", 380, 100);
 			g.setFont(instructionsFont);
 			g.drawString("Press ENTER to start", 570, 300);
 			g.drawString("Press SPACE for instructions", 530, 400);
-			
+
 		} else {
 			g.setColor(Color.BLUE);
 			g.fillRect(0, 0, WIDTH, HEIGHT);
 		}
 
-
 	}
 
 	void drawGameState(Graphics g) {
-		
+		g.drawImage(image, 0, 0, Game.WIDTH, Game.HEIGHT, null);
+		g.drawImage(image3, 400, 600, 200, 200, null);
 	}
 
 	void drawEndState(Graphics g) {
@@ -109,34 +107,42 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 			g.fillRect(0, 0, WIDTH, HEIGHT);
 		}
 
-
-
-
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		repaint();
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(currentState==MENU) {
+		if (currentState == MENU) {
 			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-				
-					JOptionPane.showMessageDialog(null, "Welcome to the Game. This dog has lost its owner and has to return safely. In order to do so, it has to cross the river, avoid obstacles while running in the fields, and identify its owner. Good luck!!");
-				}
+			
+				JOptionPane.showMessageDialog(null,
+						"Welcome to the Game. This dog has lost its owner and has to return safely. In order to do so, it has to cross the river, avoid obstacles while running in the fields, and identify its owner. Good luck!!");
 			}
+		
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+			currentState++;
+			System.out.println(currentState);
+		}
+		repaint();
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
